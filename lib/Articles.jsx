9baaa045548a05f3c673d7e79508871e48a0e63b
Dylan Avery <fossilfuelmusic@gmail.com>
2016-12-29 18:21:36 -0700

    slowly but surely these cards for these articles, might need some advice on large bodies of text alternating, helper file

--- lib/Articles.jsx
+++ lib/Articles.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Card from './Card'
 
 
 export default class Articles extends React.Component {
@@ -7,6 +8,13 @@ export default class Articles extends React.Component {
     return (
     <article className='articles'>
       <h1 className='underlines'>ARTICLES</h1>
+      <Card title='Sweet Sleep' preview='“Early to sleep
+      and early to rise makes a man health, wealthy
+      and wise.” says the 1735 edition of Poor
+      Richards Almanack. Not surprisingly,
+      it’s often attributed to Benjamin Franklin,
+      who was 29 years old at the time.' />
+      <Card />
     </article>
 )
   }
--- lib/Card.jsx
+++ lib/Card.jsx
@@ -5,12 +5,11 @@ export default class Card extends React.Component {
 
   render() {
     return (
-    <section>
-      <h1>Article Name</h1>
-      <h2>IMG</h2>
-      <h2>First sentence...</h2>
-      <h2> Show More</h2>
-      <h2>Link</h2>
+    <section className='card'>
+      <h1>{this.props.title}</h1>
+      <h2>{this.props.preview}</h2>
+      <p>Show More?</p>
+      <h2>{this.props.url}</h2>
       {/* SHOW NEXT 2? RIGHT LEFT BUTTONS */}
     </section>
 )
