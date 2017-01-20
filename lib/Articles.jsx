4928edbdc0921f8a2aa121342f64edfd637e59a4
Dylan Avery <fossilfuelmusic@gmail.com>
2017-01-20 13:06:01 -0700

    adding article content painstakingly

--- lib/Articles.jsx
+++ lib/Articles.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Card from './Card'
-import sweetsleep from './helpers/ArticleContent'
+import SweetSleep from './helpers/SweetSleep'
+import Pace from './helpers/Pace'
 
 export default class Articles extends React.Component {
 
@@ -8,15 +9,14 @@ export default class Articles extends React.Component {
     return (
     <article className='articles'>
       <h1 id='article' className='underlines'>ARTICLES</h1>
-      <Card title='Sweet Sleep'
+      <SweetSleep title='Sweet Sleep'
       preview='“Early to sleep
       and early to rise makes a man health, wealthy
       and wise.” says the 1735 edition of Poor
       Richards Almanack. Not surprisingly,
       it’s often attributed to Benjamin Franklin,
-      who was 29 years old at the time.'
-      full={sweetsleep} />
-      <Card title='Access to Health Care – The PACE Program' preview='The goal of PACE is to be very patient-focused and manage well the complex medical, functional, and social needs of the frail elderly, thus keeping people in their homes for as long as possible, avoiding more expensive and often less desirable settings.'/>
+      who was 29 years old at the time.'/>
+      <Pace title='Access to Health Care – The PACE Program' preview='The goal of PACE is to be very patient-focused and manage well the complex medical, functional, and social needs of the frail elderly, thus keeping people in their homes for as long as possible, avoiding more expensive and often less desirable settings.'/>
     </article>
 )
   }
