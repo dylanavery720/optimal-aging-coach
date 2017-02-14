8157ef85a0c55e86cdfa83c508ff5d6362e5786f
Dylan Avery <fossilfuelmusic@gmail.com>
2017-02-14 09:07:04 -0700

    bundling

--- main.bundle.js
+++ main.bundle.js
@@ -8242,7 +8242,8 @@
 	      _react2.default.createElement(_reactRouter.Route, { path: '/pace', component: _Pace2.default }),
 	      _react2.default.createElement(_reactRouter.Route, { path: '/aging', component: _Aging2.default }),
 	      _react2.default.createElement(_reactRouter.Route, { path: '/medications', component: _Medications2.default })
-	    )
+	    ),
+	    's'
 	  )
 	), document.getElementById('application'));
 
@@ -34825,10 +34826,10 @@
 	          'div',
 	          null,
 	          _react2.default.createElement(_Card2.default, { title: 'Sweet Sleep',
-	            preview: '\u201CEarly to sleep and early to rise makes a man health, wealthy and wise.\u201D says the 1735 edition of Poor Richards Almanack. Not surprisingly, it\u2019s often attributed to Benjamin Franklin, who was 29 years old at the time.', src: '/sweetsleep' }),
-	          _react2.default.createElement(_Card2.default, { title: 'Access to Health Care \u2013 The PACE Program', preview: 'The goal of PACE is to be very patient-focused and manage well the complex medical, functional, and social needs of the frail elderly, thus keeping people in their homes for as long as possible, avoiding more expensive and often less desirable settings.', src: '/pace' }),
-	          _react2.default.createElement(_Card2.default, { title: 'Lets have a conversation about aging', preview: 'The whole world is getting older. Japan is arguably the oldest country with almost 1 in 4 citizens over the age of 65. Monaco\u2019s percentage is higher but the tiny principality doesn\u2019t count as its numbers are skewed by retired rich folks from elsewhere. The U.S., with 13.3% of its people over the age of 65, doesn\u2019t even make the top 10 list: We\u2019re number eleven. More than 10,000 U.S. citizens turn 65 every day! ', src: '/aging' }),
-	          _react2.default.createElement(_Card2.default, { title: 'Medications', preview: 'Before I begin this discussion of medications, hospitals and doctors, I must beg your indulgence. When I tell someone I\u2019m a gerontologist, more often than not there\u2019s a blank look on their face. I\u2019m not offended or put off by this. Gerontology is a science and a profession that most people don\u2019t need to be familiar with.', src: '/medications' })
+	            preview: '\u201CEarly to sleep and early to rise makes a man health, wealthy and wise.\u201D', src: '/sweetsleep' }),
+	          _react2.default.createElement(_Card2.default, { title: 'Access to Health Care \u2013 The PACE Program', preview: 'The goal of PACE is to be very patient-focused and manage well the complex medical, functional, and social needs of the frail elderly', src: '/pace' }),
+	          _react2.default.createElement(_Card2.default, { title: 'Lets have a conversation about aging', preview: 'The whole world is getting older. Japan is arguably the oldest country with almost 1 in 4 citizens over the age of 65.', src: '/aging' }),
+	          _react2.default.createElement(_Card2.default, { title: 'Medications', preview: 'Before I begin this discussion of medications, hospitals and doctors, I must beg your indulgence.', src: '/medications' })
 	        )
 	      );
 	    }
